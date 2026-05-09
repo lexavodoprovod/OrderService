837bdbb97754c1cb8ccfe8fb947827395663605a
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-05-09 07:24:38 +0300

    refactor: add new fields in PaymentEventDto

--- src/main/java/com/innowise/orderservice/dto/kafka/PaymentEventDto.java
+++ src/main/java/com/innowise/orderservice/dto/kafka/PaymentEventDto.java
@@ -1,9 +1,12 @@
 package com.innowise.orderservice.dto.kafka;
 
 
+import com.innowise.orderservice.entity.EventType;
 import com.innowise.orderservice.entity.PaymentStatus;
 
 public record PaymentEventDto(
+        EventType eventType,
+        String paymentId,
         Long orderId,
         PaymentStatus status
 ) {}
---
+++ src/main/java/com/innowise/orderservice/entity/EventType.java
@@ -0,0 +1,5 @@
+package com.innowise.orderservice.entity;
+
+public enum EventType {
+    CREATE_PAYMENT
+}
